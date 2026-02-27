f7625b00af9911814e832b007cd462c5cf4451df
Henrique Reis <126351507+Henreis90@users.noreply.github.com>
2026-02-27 20:17:41 -0300

    Update variables.tf

--- modules/network/variables.tf
+++ modules/network/variables.tf
@@ -1,1 +1,16 @@
+variable "project" {
+  type = string
+}
 
+variable "env" {
+  type = string
+}
+
+variable "region" {
+  type = string
+}
+
+variable "vpc_cidr" {
+  type    = string
+  default = "10.0.0.0/16"
+}
