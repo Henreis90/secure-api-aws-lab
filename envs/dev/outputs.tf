d48dc7f87cb0f49a80f572fac064bd5afaea6be3
Henrique Reis <126351507+Henreis90@users.noreply.github.com>
2026-02-27 20:14:04 -0300

    Create outputs.tf

---
+++ envs/dev/outputs.tf
@@ -0,0 +1,75 @@
+#########################################
+# Network Outputs
+#########################################
+
+output "vpc_id" {
+  description = "ID of the VPC"
+  value       = module.network.vpc_id
+}
+
+output "public_subnet_ids" {
+  description = "Public subnet IDs"
+  value       = module.network.public_subnet_ids
+}
+
+output "private_subnet_ids" {
+  description = "Private subnet IDs"
+  value       = module.network.private_subnet_ids
+}
+
+#########################################
+# ECS / ALB Outputs
+#########################################
+
+output "alb_dns_name" {
+  description = "DNS name of the Application Load Balancer"
+  value       = module.ecs.alb_dns_name
+}
+
+output "alb_arn" {
+  description = "ARN of the Application Load Balancer"
+  value       = module.ecs.alb_arn
+}
+
+output "ecs_cluster_name" {
+  description = "ECS Cluster Name"
+  value       = module.ecs.cluster_name
+}
+
+output "ecs_service_name" {
+  description = "ECS Service Name"
+  value       = module.ecs.service_name
+}
+
+#########################################
+# RDS Outputs
+#########################################
+
+output "rds_endpoint" {
+  description = "RDS endpoint"
+  value       = module.rds.endpoint
+  sensitive   = true
+}
+
+output "rds_db_name" {
+  description = "Database name"
+  value       = module.rds.db_name
+}
+
+#########################################
+# WAF
+#########################################
+
+output "waf_web_acl_arn" {
+  description = "WAF Web ACL ARN"
+  value       = module.waf.web_acl_arn
+}
+
+#########################################
+# Logging
+#########################################
+
+output "cloudwatch_log_group" {
+  description = "Application CloudWatch log group"
+  value       = module.logging.app_log_group_name
+}
