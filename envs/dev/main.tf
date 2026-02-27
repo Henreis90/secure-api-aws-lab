25a1ef86e8da4590c5c004ecd1e734b9cf7dbea8
Henrique Reis <126351507+Henreis90@users.noreply.github.com>
2026-02-27 20:12:22 -0300

    Create main.tf

---
+++ envs/dev/main.tf
@@ -0,0 +1,41 @@
+module "network" {
+  source  = "../../modules/network"
+  project = var.project
+  env     = var.env
+  region  = var.aws_region
+}
+
+module "logging" {
+  source  = "../../modules/logging"
+  project = var.project
+  env     = var.env
+}
+
+module "rds" {
+  source             = "../../modules/rds"
+  project            = var.project
+  env                = var.env
+  vpc_id             = module.network.vpc_id
+  private_subnet_ids = module.network.private_subnet_ids
+  app_sg_id          = module.ecs.app_sg_id
+}
+
+module "ecs" {
+  source             = "../../modules/ecs"
+  project            = var.project
+  env                = var.env
+  vpc_id             = module.network.vpc_id
+  private_subnet_ids = module.network.private_subnet_ids
+  public_subnet_ids  = module.network.public_subnet_ids
+  log_group_name     = module.logging.app_log_group_name
+
+  db_endpoint = module.rds.endpoint
+  db_name     = module.rds.db_name
+}
+
+module "waf" {
+  source      = "../../modules/waf"
+  project     = var.project
+  env         = var.env
+  alb_arn     = module.ecs.alb_arn
+}
