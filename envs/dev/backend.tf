157428268f64bbfd449d05c4c0a269749ad56d6b
Henrique Reis <126351507+Henreis90@users.noreply.github.com>
2026-02-28 13:37:20 -0300

    Update backend.tf

--- envs/dev/backend.tf
+++ envs/dev/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "terraform-state"
+    bucket         = "secure-api-lab-terraform-state"
     key            = "secure-api/lab/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-locks"
