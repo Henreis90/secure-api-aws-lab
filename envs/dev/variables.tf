768bd0fab58b191425bc9baeee06fa6136758d2f
Henrique Reis <126351507+Henreis90@users.noreply.github.com>
2026-02-28 14:40:33 -0300

    Update variables.tf

--- envs/dev/variables.tf
+++ envs/dev/variables.tf
@@ -1,3 +1,7 @@
 variable "aws_region" { type = string }
 variable "project"    { type = string }
 variable "env"        { type = string }
+variable "db_password" {
+  type      = string
+  sensitive = true
+}
