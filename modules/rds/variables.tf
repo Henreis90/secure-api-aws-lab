8754b7a5ead908a9eca8d439702c85bb6dfa75a1
Henrique Reis <126351507+Henreis90@users.noreply.github.com>
2026-02-28 14:34:40 -0300

    Create variables.tf

---
+++ modules/rds/variables.tf
@@ -0,0 +1,59 @@
+variable "project" { type = string }
+variable "env"     { type = string }
+
+variable "vpc_id" { type = string }
+
+variable "private_subnet_ids" {
+  type = list(string)
+}
+
+# Security group of the application (ECS tasks) that will connect to the DB
+variable "app_sg_id" {
+  type = string
+}
+
+variable "db_name" {
+  type    = string
+  default = "appdb"
+}
+
+variable "db_username" {
+  type    = string
+  default = "appuser"
+}
+
+# For lab simplicity. You can rotate later.
+variable "db_password" {
+  type      = string
+  sensitive = true
+}
+
+variable "instance_class" {
+  type    = string
+  default = "db.t4g.micro"
+}
+
+variable "allocated_storage" {
+  type    = number
+  default = 20
+}
+
+variable "engine_version" {
+  type    = string
+  default = "16.3"
+}
+
+variable "backup_retention_days" {
+  type    = number
+  default = 1
+}
+
+variable "deletion_protection" {
+  type    = bool
+  default = false
+}
+
+variable "skip_final_snapshot" {
+  type    = bool
+  default = true
+}
