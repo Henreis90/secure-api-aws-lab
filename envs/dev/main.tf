60367cde5064e68c06c73e811e8d2b6a6cda6110
Henrique Reis <126351507+Henreis90@users.noreply.github.com>
2026-03-17 12:08:55 -0300

    Update main.tf

--- envs/dev/main.tf
+++ envs/dev/main.tf
@@ -39,3 +39,34 @@ module "waf" {
   env         = var.env
   alb_arn     = module.ecs.alb_arn
 }
+
+resource "aws_acm_certificate" "api" {
+  domain_name       = var.api_domain_name
+  validation_method = "DNS"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_route53_record" "cert_validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.api.domain_validation_options :
+    dvo.domain_name => {
+      name   = dvo.resource_record_name
+      type   = dvo.resource_record_type
+      record = dvo.resource_record_value
+    }
+  }
+
+  zone_id = var.route53_zone_id
+  name    = each.value.name
+  type    = each.value.type
+  ttl     = 60
+  records = [each.value.record]
+}
+
+resource "aws_acm_certificate_validation" "api" {
+  certificate_arn         = aws_acm_certificate.api.arn
+  validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
+}
