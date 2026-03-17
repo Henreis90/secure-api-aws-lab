f31805206ce543fd8bb38ed3082417fe9a2d904b
Henrique Reis <126351507+Henreis90@users.noreply.github.com>
2026-03-17 12:11:33 -0300

    Update main.tf

--- modules/ecs/main.tf
+++ modules/ecs/main.tf
@@ -213,3 +213,40 @@ resource "aws_ecs_service" "this" {
 
   tags = merge(local.common_tags, { Name = "${local.name}-svc" })
 }
+
+#########################################
+# Listener HTTP -> redirect
+#########################################
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.this.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type = "redirect"
+
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
+}
+
+#########################################
+# Listener HTTPS
+#########################################
+
+resource "aws_lb_listener" "https" {
+  load_balancer_arn = aws_lb.this.arn
+  port              = 443
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  certificate_arn   = var.certificate_arn
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.this.arn
+  }
+}
