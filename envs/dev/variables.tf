4147261fdad1efab31bd649843285192b4673a8b
Henrique Reis <126351507+Henreis90@users.noreply.github.com>
2026-03-17 12:04:58 -0300

    Update variables.tf

--- envs/dev/variables.tf
+++ envs/dev/variables.tf
@@ -5,3 +5,9 @@ variable "db_password" {
   type      = string
   sensitive = true
 }
+variable "api_domain_name" {
+  default = "api.cibita.com.br"
+}
+variable "route53_zone_id" {
+  default = "Z07365072A02CW8J97LV3"
+}
